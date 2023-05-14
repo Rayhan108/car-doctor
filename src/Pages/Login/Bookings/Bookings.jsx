8c224b8ca0e920064e88fdb5618e197297fe7655
Rayhan Shorker <raihanshorker56@gmail.com>
2023-05-14 16:08:21 +0600

    fixed error when token is expired

--- src/Pages/Login/Bookings/Bookings.jsx
+++ src/Pages/Login/Bookings/Bookings.jsx
@@ -1,87 +1,104 @@
 import { useContext, useEffect, useState } from "react";
 import { authContext } from "../../../Providers/AuthProvider";
 import BookingList from "./BookingList";
+import { useNavigate } from "react-router-dom";
 
 const Bookings = () => {
   const { user } = useContext(authContext);
-  const [bookings,setBookings]=useState([])
+  const [bookings, setBookings] = useState([]);
+  const navigate = useNavigate();
   const url = `http://localhost:5000/bookings?email=${user.email}`;
 
   useEffect(() => {
-    fetch(url)
+    fetch(url, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("access-token")}`,
+      },
+    })
       .then((res) => res.json())
-      .then((data) => setBookings(data));
-  }, [url]);
-  const handleDelete=(id)=>{
-    const proceed =confirm('Are you sure ,you want to delete');
-    if(proceed){
-        fetch(`http://localhost:5000/bookings/${id}`,{
-            method:"DELETE"
-        })
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-            if(data.deletedCount>0){
-                alert(' Succesfully Deleted')
-                const remaining = bookings.filter(booking=>booking._id !== id);
-                setBookings(remaining)
-            }
-        })
-    }
-    }
-    const handleBookingConfirm=id=>{
-      fetch(`http://localhost:5000/bookings/${id}`,{
-        method:"PATCH",
-        headers:{
-          'content-type':"application/json"
-        },
-        body:JSON.stringify({status:'confirm'})
+      .then((data) => {
+        
+        if (!data.error) {
+          setBookings(data);
+        } else {
+          navigate("/");
+        }
+      });
+  }, [url,navigate]);
+  const handleDelete = (id) => {
+    const proceed = confirm("Are you sure ,you want to delete");
+    if (proceed) {
+      fetch(`http://localhost:5000/bookings/${id}`, {
+        method: "DELETE",
       })
-      .then(res=>res.json())
-      .then(data=>{
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert(" Succesfully Deleted");
+            const remaining = bookings.filter((booking) => booking._id !== id);
+            setBookings(remaining);
+          }
+        });
+    }
+  };
+  const handleBookingConfirm = (id) => {
+    fetch(`http://localhost:5000/bookings/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ status: "confirm" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data);
-        if(data.modifiedCount>0){
-          const remaining = bookings.filter(booking=>booking._id !== id);
-          const updated= bookings.find(booking=>booking._id === id);
-          updated.status='confirm'
-          const newBooking = [updated,...remaining]
+        if (data.modifiedCount > 0) {
+          const remaining = bookings.filter((booking) => booking._id !== id);
+          const updated = bookings.find((booking) => booking._id === id);
+          updated.status = "confirm";
+          const newBooking = [updated, ...remaining];
           setBookings(newBooking);
         }
-      })
-    }
-  return <div className="space-y-10 mb-10">
-    <h1 className="text-3xl font-bold text-center">Bookings:{bookings.length}</h1>
-    <div className="overflow-x-auto w-full">
-  <table className="table w-full">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <th> Service</th>
-        <th>Customer Name</th>
-        <th>Email</th>
-        <th>Price</th>
-        <th>Status</th>
-      </tr>
-    </thead>
-    <tbody>
-     {
-      bookings.map(booking=><BookingList key={booking._id} booking={booking} handleBookingConfirm={handleBookingConfirm} handleDelete={handleDelete}></BookingList>)
-     }
-     
-    
-  
-   
-    </tbody>
-
-    
-  </table>
-</div>
-  </div>;
+      });
+  };
+  return (
+    <div className="space-y-10 mb-10">
+      <h1 className="text-3xl font-bold text-center">
+        Bookings:{bookings.length}
+      </h1>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th> Service</th>
+              <th>Customer Name</th>
+              <th>Email</th>
+              <th>Price</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking) => (
+              <BookingList
+                key={booking._id}
+                booking={booking}
+                handleBookingConfirm={handleBookingConfirm}
+                handleDelete={handleDelete}
+              ></BookingList>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Bookings;
